e7424fefcaa3885b1c7bb22c1112b4c36780982b
gandpop <noah1910nielsen@gmail.com>
2026-05-18 12:34:52 +0200

    Particle Pooling Implemented
    Hej mine Godot-Git-Godtfolk,
    
    jeg har nu implementeret den der particle pooling node. Den kan bare smides ind, der hvor det passer bedst. at den loader de forskellige partikler. Den er stand-alone, så den skal sådan set bare placeres i scenen, hvor det giver mening

---
+++ online-web-browser-test/scripts/managers/particle_pooling.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+const ARTIFACT_PARTICLES = preload("uid://b4hha0q7y1chb")
+const SMOKE_PARTICLES = preload("uid://8iaw24ps07vh")
+
+
+func _ready() -> void:
+	# Instantiating The Different Particles
+	var shine_instance = ARTIFACT_PARTICLES.instantiate()
+	var smoke_instance = SMOKE_PARTICLES.instantiate()
+	
+	# Add Particles Instance Node
+	self.get_node("instantiatedParticles").add_child(shine_instance)
+	self.get_node("instantiatedParticles").add_child(smoke_instance)
+	print("particles instantiated")
+	
+	# Emit Particles
+	shine_instance.emitting = true
+	smoke_instance.emitting = true
+	
+	
+	# Wait For Timer, Then Delete Particles Again
+	await get_tree().create_timer(5.0).timeout
+	get_node("instantiatedParticles").queue_free()
+	print("particles deleted")
